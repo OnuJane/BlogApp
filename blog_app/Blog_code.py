b745e5a34bf3d85d66b923d79e6d8983b414bac4
OnuJane <onujane05@gmail.com>
2021-04-27 08:54:18 -0700

    Add Signup and Post MOdel

---
+++ blog_app/Blog_code.py
@@ -0,0 +1,18 @@
+from django.db import models
+ 
+
+class Post(models.Model):
+    title = models.CharField(max_length=300)
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE,
+    )
+    body = models.TextField()
+    def __str__(self):
+        return self.title
+
+from django.contrib import admin
+from .models import Post
+
+admin.site.register(Post)
+
+
+    
